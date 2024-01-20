89f03138344d1e4e412ca8bb77574329a342fcb5
Safdar Sikander <safdar_sikander@live.com>
2024-01-20 10:17:18 +0500

    update version

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/version", (req, res) => {
  res.send({
-    version:"1.0.52"
+    version:"1.0.69"
  })
 });
 
